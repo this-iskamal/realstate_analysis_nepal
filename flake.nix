d75f6fb40d5a0c50f4bdc278915ee162b6aef843
ArMonarch <praffulthapa12@gmail.com>
2025-06-25 17:52:45 +0545

    feat: added needed packages along with theirs dependencies in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   # enabled.
   # This doesn't concern you if you are not using NixOS | Nix package manager
 
-  description = "Python Development Environemnt";
+  description = "Python Development Environement";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -13,17 +13,27 @@
   outputs = { nixpkgs, flake-utils, ... } :
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system;};
+        python = pkgs.python312;
+        pythonPackages = pkgs.python312Packages;
+
+        required_pythonPackages = with pythonPackages; [ polars numpy matplotlib seaborn];
 
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ python312  geckodriver curl];
+          packages = [
+            python
+          ] ++ required_pythonPackages;
 
           # NOTE: Init of zsh removes the virtual environment,
           # commenting out zsh until an solution is found
+          # Read for adding python packages that depend on other libraries:
+          # https://stackoverflow.com/questions/59594317/how-can-i-add-a-python-package-to-a-shell-nix-if-its-not-in-nixpkgs
           shellHook = ''
-            # exec zsh
+            echo "Entering Nix DevShell with Python: $(python --version) Virtual Environement"
             echo "Python : $(python --version)"
+            export PATH="${python}/bin:$PATH"
+            
             if [ -d 'venv' ]; then
               # Activate the virtual environment
               source venv/bin/activate
